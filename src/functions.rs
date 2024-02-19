7928a017abcbd28e4a2c474a7bd41f121ae4f5d5
diallo <mamadouadama609@gmail.com>
2024-02-18 22:21:47 -0600

    Fisrt commit for project setup

---
+++ src/functions.rs
@@ -0,0 +1,10 @@
+
+
+
+
+ pub fn get_full_name(first_name: &str, last_name:&str) -> String {
+
+    let full_name: String  = format!("{} {}", first_name, last_name);
+
+    return full_name;
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+
+
+mod functions;
+
+fn main() {
+
+    let full_nale  = 
+        functions::get_full_name("Diallo", "Mamadou");
+
+    println!("{}", full_nale);
+}
